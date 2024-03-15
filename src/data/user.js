b25f3be70f69c726727a8925772c72585db88892
PremMscCs <140685366+Premmsccs@users.noreply.github.com>
2024-03-16 00:58:47 +0530

    virtual_bank

---
+++ src/data/user.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    userPic: "/images/download.png",
+    name: "Prem",
+    email: "pgcs.premkumar@gmail.com",
+    password: "secret",
+    balance: 100,
+    transactionHistory: [],
+  },
+];
+
+export default users;
